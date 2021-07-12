9ea4a94d5994466c0fab9e6124c0a5b84b3db972
r2road <studior2road@gmail.com>
2021-07-13 01:07:56 +0900

    update : actor - r2_actor_0003 - 0003

--- actor/r2_actor_0003/r2_actor_0003_0003.bat
+++ actor/r2_actor_0003/r2_actor_0003_0003.bat
@@ -2,11 +2,13 @@ cls
 
 set aseprite_file_name=%~n0.aseprite
 set export_file_name=%~n0
-set target_tag_name=wlk
+set target_tag_names=idl wlk run jmp atk
 set scale_amount=8
 
+(for %%t in (%target_tag_names%) do ( 
 call ../../asepriteExporter_GIF_LayerMerge_Title_Tag.bat^
  %aseprite_file_name%^
  %export_file_name%^
- %target_tag_name%^
+ %%t^
  %scale_amount%
+))
