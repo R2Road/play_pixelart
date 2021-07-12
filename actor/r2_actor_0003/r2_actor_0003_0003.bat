533902f30461f71064086a144b82e48d605c55c5
r2road <studior2road@gmail.com>
2021-07-13 00:22:48 +0900

    add : actor - r2_actor_0003 - 0003

---
+++ actor/r2_actor_0003/r2_actor_0003_0003.bat
@@ -0,0 +1,12 @@
+cls
+
+set aseprite_file_name=%~n0.aseprite
+set export_file_name=%~n0
+set target_tag_name=wlk
+set scale_amount=8
+
+call ../../asepriteExporter_GIF_LayerMerge_Title_Tag.bat^
+ %aseprite_file_name%^
+ %export_file_name%^
+ %target_tag_name%^
+ %scale_amount%
