cf7148119cf7cd3d617b36cf54952f9daccea1fa
r2road <studior2road@gmail.com>
2021-07-13 01:31:06 +0900

    rename : exporter

--- actor/r2_actor_0003/r2_actor_0003_0001.bat
+++ actor/r2_actor_0003/r2_actor_0003_0001.bat
@@ -5,7 +5,7 @@ set export_file_name=%~n0
 set target_tag_name=rotate
 set scale_amount=8
 
-call ../../asepriteExporter_GIF_SingleTag_LayerMerge_Title_Tag.bat^
+call ../../asepriteExporter_GIF_SingleTag_Title_Tag.bat^
  %aseprite_file_name%^
  %export_file_name%^
  %target_tag_name%^
