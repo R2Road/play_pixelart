ff2416f54e4219dd92419770b7e291348b10c1d7
r2road <studior2road@gmail.com>
2021-07-13 01:10:16 +0900

    add : test_exporter - test_asepriteExporter_GIF_LayerMerge_Title_Tag__4Animations

---
+++ test_exporter/test_asepriteExporter_GIF_LayerMerge_Title_Tag__4Animations.bat
@@ -0,0 +1,14 @@
+cls
+
+set aseprite_file_name=%~n0.aseprite
+set export_file_name=%~n0
+set target_tag_names=idl wlk run jmp atk
+set scale_amount=8
+
+(for %%t in (%target_tag_names%) do ( 
+call ../asepriteExporter_GIF_LayerMerge_Title_Tag.bat^
+ %aseprite_file_name%^
+ %export_file_name%^
+ %%t^
+ %scale_amount%
+))
