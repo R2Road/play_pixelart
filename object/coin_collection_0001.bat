46b02028e5b56b315df0efd38f4be2f61052ee32
r2road <studior2road@gmail.com>
2021-07-15 10:11:17 +0900

    add : object - coin_collection_0001

---
+++ object/coin_collection_0001.bat
@@ -0,0 +1,10 @@
+cls
+
+set aseprite_file_name=%~n0.aseprite
+set export_file_name=%~n0
+set scale_amount=8
+
+call ../asepriteExporter_GIF_AllFrames_Title.bat^
+ %aseprite_file_name%^
+ %export_file_name%^
+ %scale_amount%
