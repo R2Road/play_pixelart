8857a2bd363f4a8523e2e9448a5f407d6c0a240c
r2road <studior2road@gmail.com>
2021-07-23 04:40:30 +0900

    update : exporter

--- asepriteExporter_PNG_LayerMerge_Title_Frame.bat
+++ asepriteExporter_PNG_LayerMerge_Title_Frame.bat
@@ -5,7 +5,7 @@ echo arg2 - %2
 "c:\Program Files (x86)\Steam\steamapps\common\Aseprite\Aseprite.exe"^
  -b^
  %1^
- --filename-format "{title}_{frame}.{extension}"^
+ --filename-format "{title}_{frame}.png"^
  --save-as %2
 
 pause
--- asepriteExporter_PNG_LayerMerge_Title_Tag_TagFrame.bat
+++ asepriteExporter_PNG_LayerMerge_Title_Tag_TagFrame.bat
@@ -5,7 +5,7 @@ echo arg2 - %2
 "c:\Program Files (x86)\Steam\steamapps\common\Aseprite\Aseprite.exe"^
  -b^
  %1^
- --filename-format "{title}_{tag}_{tagframe}.{extension}"^
+ --filename-format "{title}_{tag}_{tagframe}.png"^
  --save-as %2
 
 pause
