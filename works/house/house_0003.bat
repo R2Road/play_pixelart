7f7b5456ff0f4bd839d662b899d276f37c027416
r2road <studior2road@gmail.com>
2021-07-31 13:32:33 +0900

    update : house - house_0003

--- works/house/house_0003.bat
+++ works/house/house_0003.bat
@@ -3,7 +3,7 @@ cls
 set aseprite_file_name=%~n0.aseprite
 set export_file_name=%~n0
 set target_tag_names=idl
-set scale_amount=8
+set scale_amount=4
 
 (for %%t in (%target_tag_names%) do ( 
 call ../asepriteExporter_GIF_SingleTag_Title_Tag.bat^
