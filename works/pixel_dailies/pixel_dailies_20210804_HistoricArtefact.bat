0394151bded2109d0e2987098780811ecfee3645
r2road <studior2road@gmail.com>
2021-08-04 20:15:29 +0900

    update : pixel_dailies - 20210804

--- works/pixel_dailies/pixel_dailies_20210804_HistoricArtefact.bat
+++ works/pixel_dailies/pixel_dailies_20210804_HistoricArtefact.bat
@@ -2,7 +2,7 @@ cls
 
 set aseprite_file_name=%~n0.aseprite
 set png_file_name=%~n0.png
-set scale_amount=5
+set scale_amount=4
 
 call ../asepriteExporter_PNG_LayerMerge_Title.bat^
  %aseprite_file_name%^
