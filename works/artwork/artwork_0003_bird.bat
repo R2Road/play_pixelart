af3bded7c97454d7fd5bb5dd261dae3cd2e27790
r2road <studior2road@gmail.com>
2021-08-23 12:29:12 +0900

    update : artwork - 0003 - bird

--- works/artwork/artwork_0003_bird.bat
+++ works/artwork/artwork_0003_bird.bat
@@ -2,9 +2,11 @@ cls
 
 set aseprite_file_name=%~n0.aseprite
 set export_file_name=%~n0
+set target_tag_name=mockup
 set scale_amount=8
 
-call ../asepriteExporter_GIF_AllFrames_Title.bat^
+call ../asepriteExporter_GIF_SingleTag_Title_Tag.bat^
  %aseprite_file_name%^
  %export_file_name%^
+ %target_tag_name%^
  %scale_amount%
