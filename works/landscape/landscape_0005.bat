490faccc54ca62c44a21ba705a0a12377bda3ec9
r2road <studior2road@gmail.com>
2021-09-06 00:28:52 +0900

    update : landscape - 0005

--- works/landscape/landscape_0005.bat
+++ works/landscape/landscape_0005.bat
@@ -2,7 +2,9 @@ cls
 
 set aseprite_file_name=%~n0.aseprite
 set png_file_name=%~n0.png
+set scale_amount=6
 
 call ../asepriteExporter_PNG_LayerMerge_Title.bat^
  %aseprite_file_name%^
- %png_file_name%
+ %png_file_name%^
+ %scale_amount%
