824e387ef7946b49c9830342df0798e86c119e08
r2road <studior2road@gmail.com>
2021-09-19 17:00:43 +0900

    update : pixel_dailies - cat, restriction: 32x32px or less

--- works/pixel_dailies/pixel_dailies_20210918_cat32x32.bat
+++ works/pixel_dailies/pixel_dailies_20210918_cat32x32.bat
@@ -4,7 +4,17 @@ set aseprite_file_name=%~n0.aseprite
 set export_file_name=%~n0
 set scale_amount=4
 
-call ../asepriteExporter_GIF_AllFrames_Title.bat^
+call ../asepriteExporter_PNG_LayerMerge_FirstFrame_Title.bat^
  %aseprite_file_name%^
  %export_file_name%^
+ %scale_amount%
+
+
+
+set target_tag_name=ani
+
+call ../asepriteExporter_GIF_SingleTag_Title_Tag.bat^
+ %aseprite_file_name%^
+ %export_file_name%^
+ %target_tag_name%^
  %scale_amount%
