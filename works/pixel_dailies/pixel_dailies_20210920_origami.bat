5f875fc3ab87579be61a6c89dccc17f107e65223
r2road <studior2road@gmail.com>
2021-09-21 12:30:24 +0900

    update : pixel_dailies - origami

--- works/pixel_dailies/pixel_dailies_20210920_origami.bat
+++ works/pixel_dailies/pixel_dailies_20210920_origami.bat
@@ -3,10 +3,8 @@ cls
 set aseprite_file_name=%~n0.aseprite
 set export_file_name=%~n0
 set scale_amount=4
-set target_tag_name=ani
 
-call ../asepriteExporter_GIF_SingleTag_Title_Tag.bat^
+call ../asepriteExporter_GIF_AllFrames_Title.bat^
  %aseprite_file_name%^
  %export_file_name%^
- %target_tag_name%^
  %scale_amount%
