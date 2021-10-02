b6af7e857ca06b2b9924495f6f619a4d59a7df5e
r2road <studior2road@gmail.com>
2021-10-02 18:25:23 +0900

    ...

--- works/cat/cat_0003.bat
+++ works/cat/cat_0003.bat
@@ -1,10 +1,10 @@
 cls
 
 set aseprite_file_name=%~n0.aseprite
-set png_file_name=%~n0.png
+set export_file_name=%~n0.png
 set scale_amount=8
 
 call ../asepriteExporter_PNG_LayerMerge_Title.bat^
  %aseprite_file_name%^
- %png_file_name%^
- %scale_amount%^
+ %export_file_name%^
+ %scale_amount%
